f0beb63403c26fbca9d347bbfd79cc98025b1efd
Malcolm Peter <agtychefortuna@gmail.com>
2017-11-30 06:37:57 -0800

    Update anti_vowels.py

--- Fix em/anti_vowels.py
+++ Fix em/anti_vowels.py
@@ -1,10 +1,10 @@
 def anti_vowel(text):
-    t=""
-    for c in text:
-        for i in "ieaouIEAOU":
-            if c==i:
-                c=""
+    worked_on = ""
+    for letter in text:
+        for vowel in "ieaouIEAOU":
+            if letter== vowel:
+                letter=""
             else:
-                c=c
-        t=t+c
-    return t
+                letter = letter
+        worked_on = worked_on + letter
+    return worked_on
