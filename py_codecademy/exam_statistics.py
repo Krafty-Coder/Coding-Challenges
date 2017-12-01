d6adbdbd8d05c9660c5f7b8fa8328e93027d12a1
Malcolm Peter <agtychefortuna@gmail.com>
2017-12-01 08:15:55 -0800

    Update exam_statistics.py

--- py_codecademy/exam_statistics.py
+++ py_codecademy/exam_statistics.py
@@ -1,1 +1,17 @@
-def print_grades():
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def grades_sum(scores):
+  summ = 0
+  for score in scores:
+    summ += score
+  print summ
+  return summ
+
+grades_sum(grades)
+
+def grades_average(grades_input):
+  total = grades_sum(grades_input)
+  average = total/float(len(grades_input))
+  return average
+
+print grades_average(grades)
